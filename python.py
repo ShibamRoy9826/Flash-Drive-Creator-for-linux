637f7876417fea48b00d70ab770adad3ef7648ec
RayAntariksha <antariksharay20@gmail.com>
2024-10-16 19:05:12 +0530

    created the basic skeleton of the program

--- python.py
+++ python.py
@@ -1,11 +1,25 @@
 import os
 
 print("Type 'help' for help and 'exit'")
-input("Please insert pendrive and press enter.")
+firstin = input("Type 'help' for help. Press enter to see the available drives and continue with the installation.")
 
-x = input(">>")
-
-if x == "help":
-    print("")
+if firstin == "help":
+    print("this is help")
 else:
-    os.system(command= x)
+    os.system(command= "lsblk")
+
+#collecting data
+drive = input("Please type in the name of the Drive you want to flash: ")
+iso = input("Please enter the path to your iso file: ")
+verif = input("All data on your drive will be lost. Do you want to continue?[Y/n]: ")
+
+# Step 1:
+# Unmount drive
+os.system(command="sudo umount "+ drive)
+
+# Step 2:
+# Create the Bootable USB
+
+#print("sudo dd if=" + iso + " of="+ drive +" bs=4M status=progress")
+
+os.system(command="sudo dd if=" + iso + " of=/dev/sdX bs=4M status=progress")
