e8cf90dc78610a1a0d26cbcb219c63bdbeead2c2
RayAntariksha <antariksharay20@gmail.com>
2024-10-16 17:42:36 +0530

    Created a program to execute command-line arguments

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
-print("hello world")
+import os
+
+print("Type 'help' for help and 'exit'")
+input("Please insert pendrive and press enter.")
+
+x = input(">>")
+
+if x == "help":
+    print("")
+else:
+    os.system(command= x)
