e7f24231fc4159095bc64cd9d62409cd54f931d9
Shibam Roy <royshibam9826@gmail.com>
2024-10-30 22:29:20 +0530

    Update python.py
    Adapted to the functions-based structure

--- python.py
+++ python.py
@@ -1,35 +1,20 @@
 #!/usr/bin/python
-import os
+from functions import *
 
-print("Type 'help' for help and 'exit'")
-firstin = input("Type 'help' for help. Press enter to see the available drives and continue with the installation.")
+welcomeMessage()
 
+fistin=input()
 if firstin == "help":
-    print("this is help")
+    helpMessage()
 else:
-    os.system(command= "lsblk")
+    run("lsblk")
 
-#collecting data
+# Taking user inputs
 drive = input("Please type in the name of the Drive you want to flash: ")
 iso = input("Please enter the path to your iso file: ")
 verif = input("All data on your drive will be lost. Do you want to continue?[Y/n]: ")
 
 if verif == "Y":
-
-    # Step 1:
-    # Unmount drive
-    os.system(command="sudo umount "+ drive)
-
-    # Step 2:
-    # Create the Bootable USB
-
-    #print("sudo dd if=" + iso + " of="+ drive +" bs=4M status=progress")
-
-    os.system(command="sudo dd if=" + iso + " of=/dev/sdX bs=4M status=progress")
-
-    # Step 4:
-    # Sync and eject
-    os.system(command="sudo sync")
-
+    createBootable(drive,iso)
 else:
     print("...................TASK ABORTED.....................")
